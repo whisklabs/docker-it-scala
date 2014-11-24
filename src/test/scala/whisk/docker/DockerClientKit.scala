bc134bc05598e28068a3e0a50556994cd147b416
Dmitry Kurinskiy <name.alari@gmail.com>
2014-11-24 15:01:33 +0300

    default config works correctly when all required ENVs are set

--- src/test/scala/whisk/docker/DockerClientKit.scala
+++ src/test/scala/whisk/docker/DockerClientKit.scala
@@ -6,9 +6,6 @@ import com.github.dockerjava.core.{ DockerClientBuilder, DockerClientConfig => D
 trait DockerClientKit extends DockerClientConfig {
   implicit override val dockerClient: DockerClient = {
     val dockerConfig = DCC.createDefaultConfigBuilder()
-      .withUri("https://192.168.59.103:2376")
-      .withDockerCertPath("/Users/alari/.boot2docker/certs/boot2docker-vm")
-      .build()
     DockerClientBuilder.getInstance(dockerConfig).build()
   }
 }
