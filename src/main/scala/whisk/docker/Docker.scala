7647de14464f184b9c6dfc5cb7c392cb5b637eac
Viktor Taranenko <viktor@whisk.co.uk>
2014-12-21 20:40:32 +0000

    explicit docker cmd run impl

--- src/main/scala/whisk/docker/Docker.scala
+++ src/main/scala/whisk/docker/Docker.scala
@@ -1,9 +1,10 @@
 package whisk.docker
 
 import com.github.dockerjava.core.{ DockerClientBuilder, DockerClientConfig => DCC }
+import com.github.dockerjava.jaxrs.DockerCmdExecFactoryImpl
 
 class Docker(val config: DCC) {
-  lazy val client = DockerClientBuilder.getInstance(config).build()
+  lazy val client = DockerClientBuilder.getInstance(config).withDockerCmdExecFactory(new DockerCmdExecFactoryImpl).build()
 
   lazy val host = config.getUri.getHost
 }
