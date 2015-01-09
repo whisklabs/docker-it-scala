2d980b6c7a620574bc8419f24edb58111988154c
Dmitry Kurinskiy <name.alari@gmail.com>
2015-01-09 19:31:53 +0300

    syncdelay = 0

--- src/main/scala/whisk/docker/DockerMongodbService.scala
+++ src/main/scala/whisk/docker/DockerMongodbService.scala
@@ -7,7 +7,7 @@ trait DockerMongodbService extends DockerKit {
   val mongodbContainer = DockerContainer("mongo:2.6.5")
     .withPorts(DefaultMongodbPort -> None)
     .withReadyChecker(DockerReadyChecker.LogLine(_.contains("waiting for connections on port")))
-    .withCommand("mongod", "--nojournal", "--smallfiles")
+    .withCommand("mongod", "--nojournal", "--smallfiles", "--syncdelay", "0")
 
   abstract override def dockerContainers: List[DockerContainer] = mongodbContainer :: super.dockerContainers
 }
