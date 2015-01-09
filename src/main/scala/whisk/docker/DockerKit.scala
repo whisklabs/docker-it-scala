f78144b5c4279c1bf5820dfba6a8b229592e8425
Viktor Taranenko <viktor@whisk.co.uk>
2015-01-09 23:30:31 +0000

    optimise docker images pulling

--- src/main/scala/whisk/docker/DockerKit.scala
+++ src/main/scala/whisk/docker/DockerKit.scala
@@ -31,11 +31,23 @@ trait DockerKit {
 
   def dockerContainers: List[DockerContainer] = Nil
 
+  def listImages(): Future[Set[String]] = {
+    import scala.collection.JavaConverters._
+    Future(docker.client.listImagesCmd().exec().asScala.flatMap(_.getRepoTags).toSet)
+  }
+
   def stopRmAll(): Future[Seq[DockerContainer]] =
     Future.traverse(dockerContainers)(_.remove(force = true))
 
-  def pullImages(): Future[Seq[DockerContainer]] =
-    Future.traverse(dockerContainers)(_.pull())
+  def pullImages(): Future[Seq[DockerContainer]] = {
+    listImages().flatMap { images =>
+      val containersToPull = dockerContainers.filterNot { c =>
+        val cImage = if (c.image.contains(":")) c.image else c.image + ":latest"
+        images(cImage)
+      }
+      Future.traverse(containersToPull)(_.pull())
+    }
+  }
 
   def initReadyAll(): Future[Seq[(DockerContainer, Boolean)]] =
     Future.traverse(dockerContainers)(_.init()).flatMap(Future.traverse(_)(c => c.isReady().map(c -> _).recover {
