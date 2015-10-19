8908a8a6074b527763a16fc9b55e0c26cfbce823
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2015-10-19 12:45:02 -0400

    add redis service

---
+++ core/src/main/scala/com/whisk/docker/DockerRedisService.scala
@@ -0,0 +1,12 @@
+package com.whisk.docker
+
+trait DockerRedisService extends DockerKit {
+
+  val DefaultRedisPort = 6379
+
+  val redisContainer = DockerContainer("dockerfile/redis:3.0.5")
+    .withPorts(DefaultRedisPort -> None)
+    .withReadyChecker(DockerReadyChecker.LogLineContains("Server started"))
+
+  abstract override def dockerContainers = redisContainer :: super.dockerContainers
+}
