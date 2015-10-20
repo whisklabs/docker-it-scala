d98a64437a36c2767ca8cbcaaadaf25ab61ef395
Yoel Garcia Diaz <yoel.garciadiaz@razorfish.com>
2015-10-20 10:19:42 -0400

    add redis-sentinel cmd

--- core/src/main/scala/com/whisk/docker/DockerRedisSentinelService.scala
+++ core/src/main/scala/com/whisk/docker/DockerRedisSentinelService.scala
@@ -7,6 +7,7 @@ trait DockerRedisSentinelService extends DockerKit {
   val redisSentinelContainer = DockerContainer("joshula/redis-sentinel")
     .withPorts(DefaultRedisPort -> None)
     .withReadyChecker(DockerReadyChecker.LogLineContains("monitor master mymaster"))
+    .withCommand("--sentinel announce-ip localhost", s"--sentinel announce-port $DefaultRedisPort")
 
   abstract override def dockerContainers = redisSentinelContainer :: super.dockerContainers
 }
