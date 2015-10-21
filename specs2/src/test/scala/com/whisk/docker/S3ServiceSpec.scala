a508d2217b0f216101fca3bcc19c8744496f442d
tim <glidester@gmail.com>
2015-10-21 15:28:58 +0100

    Adding Spec2 version of sample it test

---
+++ specs2/src/test/scala/com/whisk/docker/S3ServiceSpec.scala
@@ -0,0 +1,19 @@
+package com.whisk.docker
+
+import com.whisk.docker.specs2.DockerTestKit
+import org.specs2._
+import org.specs2.specification.core.Env
+import scala.concurrent._
+
+class S3ServiceSpec(env: Env) extends Specification
+    with DockerElasticsearchService
+    with DockerTestKit {
+
+  implicit val ee = env.executionEnv
+
+  def is = s2"""
+  The elasticsearch container should be ready $x1
+                                              """
+
+  def x1 = elasticsearchContainer.isReady() must beTrue.await
+}
