627acd41a4646b91bc76f1a486b5ccfe5738a0f5
Viktor Taranenko <viktortnk@gmail.com>
2016-05-11 22:39:31 +0100

    example using Netty in mongo spec

--- core/src/main/scala/com/whisk/docker/Docker.scala
+++ core/src/main/scala/com/whisk/docker/Docker.scala
@@ -1,9 +1,10 @@
 package com.whisk.docker
 
+import com.github.dockerjava.api.command.DockerCmdExecFactory
 import com.github.dockerjava.core.{DockerClientBuilder, DockerClientConfig}
 import com.github.dockerjava.jaxrs.DockerCmdExecFactoryImpl
 
-class Docker(val config: DockerClientConfig) {
-  val client = DockerClientBuilder.getInstance(config).withDockerCmdExecFactory(new DockerCmdExecFactoryImpl).build()
+class Docker(val config: DockerClientConfig, factory: DockerCmdExecFactory = new DockerCmdExecFactoryImpl) {
+  val client = DockerClientBuilder.getInstance(config).withDockerCmdExecFactory(factory).build()
   val host = config.getDockerHost.getHost
 }
--- scalatest/src/test/scala/com/whisk/docker/MongodbServiceSpec.scala
+++ scalatest/src/test/scala/com/whisk/docker/MongodbServiceSpec.scala
@@ -1,15 +1,21 @@
 package com.whisk.docker
 
+import com.github.dockerjava.core.DockerClientConfig
+import com.github.dockerjava.netty.DockerCmdExecFactoryImpl
 import com.whisk.docker.scalatest.DockerTestKit
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.time.{ Second, Seconds, Span }
-import org.scalatest.{ BeforeAndAfterAll, FlatSpec, Matchers }
+import org.scalatest.time.{Second, Seconds, Span}
+import org.scalatest.{FlatSpec, Matchers}
 
 class MongodbServiceSpec extends FlatSpec with Matchers
-    with DockerTestKit
-    with DockerMongodbService {
+  with DockerTestKit
+  with DockerMongodbService {
 
   implicit val pc = PatienceConfig(Span(20, Seconds), Span(1, Second))
+
+  override implicit val docker: Docker = new Docker(
+    DockerClientConfig.createDefaultConfigBuilder().build(),
+    new DockerCmdExecFactoryImpl
+  )
 
   "mongodb node" should "be ready with log line checker" in {
     isContainerReady(mongodbContainer).futureValue shouldBe true
