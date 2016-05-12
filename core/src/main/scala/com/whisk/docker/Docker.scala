218412038e5fa7e41e72a36842b808292bbd111d
Viktor Taranenko <viktortnk@gmail.com>
2016-05-12 11:16:06 +0100

    Use Netty transport by default

--- core/src/main/scala/com/whisk/docker/Docker.scala
+++ core/src/main/scala/com/whisk/docker/Docker.scala
@@ -2,7 +2,7 @@ package com.whisk.docker
 
 import com.github.dockerjava.api.command.DockerCmdExecFactory
 import com.github.dockerjava.core.{DockerClientBuilder, DockerClientConfig}
-import com.github.dockerjava.jaxrs.DockerCmdExecFactoryImpl
+import com.github.dockerjava.netty.DockerCmdExecFactoryImpl
 
 class Docker(val config: DockerClientConfig, factory: DockerCmdExecFactory = new DockerCmdExecFactoryImpl) {
   val client = DockerClientBuilder.getInstance(config).withDockerCmdExecFactory(factory).build()
