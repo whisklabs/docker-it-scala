0627ec638febbdababccf1020afe24a9007e3c56
Viktor Taranenko <viktortnk@gmail.com>
2016-09-15 09:30:46 +0100

    make docker-java fallback to loopbackAddress if host is null

--- core/src/main/scala/com/whisk/docker/Docker.scala
+++ core/src/main/scala/com/whisk/docker/Docker.scala
@@ -1,5 +1,7 @@
 package com.whisk.docker
 
+import java.net.InetAddress
+
 import com.github.dockerjava.api.command.DockerCmdExecFactory
 import com.github.dockerjava.core.{DockerClientBuilder, DockerClientConfig}
 import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory
@@ -7,5 +9,5 @@ import com.github.dockerjava.jaxrs.JerseyDockerCmdExecFactory
 class Docker(val config: DockerClientConfig,
             protected val factory: DockerCmdExecFactory = new JerseyDockerCmdExecFactory) {
   val client = DockerClientBuilder.getInstance(config).withDockerCmdExecFactory(factory).build()
-  val host = config.getDockerHost.getHost
+  val host = Option(config.getDockerHost.getHost).getOrElse(InetAddress.getLoopbackAddress.getHostAddress)
 }
