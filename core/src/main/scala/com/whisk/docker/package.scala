31293d3c31ce43fede59571fba26cbe67d720bfd
Alessandro Vermeulen <github@spockz.nl>
2017-01-09 22:21:40 +0100

    Ran scalaformat.

--- core/src/main/scala/com/whisk/docker/package.scala
+++ core/src/main/scala/com/whisk/docker/package.scala
@@ -1,12 +1,12 @@
 package com.whisk
 
 /**
- * General utility functions
- */
+  * General utility functions
+  */
 package object docker {
   implicit class OptionalOps[A](val content: A) extends AnyVal {
     def withOption[B](optional: Option[B])(f: (A, B) => A): A = optional match {
-      case None    => content
+      case None => content
       case Some(x) => f(content, x)
     }
   }
